c3afbebb35abce580d0d6ef2a65509775385d791
Sarang Mittal <sm.sarang09@gmail.com>
2025-08-30 21:39:55 +0530

    added recommendation queue logic for posts

--- backend/modules/firebase_config.py
+++ backend/modules/firebase_config.py
@@ -1,16 +1,19 @@
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+from firebase_admin import credentials, db
 from pathlib import Path
 
-
-#return root reference of the firebase database upon verifying the credentials for admin
-def init_firebase():
-    key_path = Path(__file__).resolve().parent.parent/"data"/"serviceAccountKey.json"
+# Initialize only once when this file is imported
+if not firebase_admin._apps:
+    key_path = Path(__file__).resolve().parent.parent / "data" / "serviceAccountKey.json"
     cred = credentials.Certificate(key_path)
-
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://se-app-dev-9e20f-default-rtdb.asia-southeast1.firebasedatabase.app/"
     })
 
+
+def init_firebase():
     return db.reference('/posts/')
+
+
+def get_users_ref():
+    return db.reference('/users/')
