35eef42e42cdc612c1513152a2587eea5b112359
Piyush <piyushkatkar9421@gmail.com>
2025-08-31 17:07:00 +0530

    fix svg rendering and adapt with dark mode

--- frontend/lib/widgets/post_card.dart
+++ frontend/lib/widgets/post_card.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:frontend/models/post_data.dart';
 
 class PostCard extends StatelessWidget {
+  const PostCard({super.key, required this.post});
   final PostData post;
 
-  const PostCard({super.key, required this.post});
-
+  Widget buildPostImage(String url, BuildContext context) {
+    if (url.toLowerCase().endsWith('.svg')) {
+    return Center(
+      child: SvgPicture.network(
+        url,
+        height: 250,
+        colorFilter: ColorFilter.mode(
+          Theme.of(context).colorScheme.onSurface, 
+          BlendMode.srcIn
+        ),
+        placeholderBuilder: (context) => const Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+    } else {
+      return Image.network(
+        url,
+        fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+          return const Center(child: CircularProgressIndicator());
+        },
+        errorBuilder: (context, error, stackTrace) {
+          return Image.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.cover,
+          );
+        },
+      );
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -14,14 +46,18 @@ class PostCard extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(16),
       ),
+
       child: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Image
+            buildPostImage(post.image_link, context),
+
             // Title
             Text(
-              post.title,
+              post.title.trim(),
               style: const TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
@@ -51,19 +87,12 @@ class PostCard extends StatelessWidget {
 
             // Footer row: likes + data
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
-                  children: [
-                    const Icon(Icons.favorite, size: 18, color: Colors.red),
-                    const SizedBox(width: 4),
-                    Text("${post.likes}"),
-                  ],
-                ),
-                // Text(
-                //   post.data, // assuming this is like a date string
-                //   style: const TextStyle(color: Colors.grey),
-                // ),
+                const Icon(Icons.favorite, size: 25, color: Colors.red),
+                const SizedBox(width: 4),
+                Text("${post.likes}"),
+                const SizedBox(width: 4),
+                const Icon(Icons.bookmark, size: 25, color: Colors.blue),
               ],
             ),
           ],
