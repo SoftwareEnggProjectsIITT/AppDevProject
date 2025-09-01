26edc36821011fba8a952269520b9472d9d1cef7
Sarang Mittal <sm.sarang09@gmail.com>
2025-09-01 21:27:40 +0530

    find images in twitter as well for post

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ import modules.scraper as scraper
 import modules.uploader as uploader
 
 #getting all the links for the specified date
-date = "27"
+date = "1"
 category_ids_dict = get_ids.get_ids_by_categories(date)
 
 ref = firebase_config.init_firebase()
@@ -17,10 +17,10 @@ for category in category_ids_dict:
     
     for id in list:
         html = scraper.fetch_page(id)   #here html is of type BeautifulSoup and not string
-        
+                
         title = parser_utils.parse_title(html)
         body = parser_utils.parse_body(html)
         date = parser_utils.parse_date(html)
-        image_link = parser_utils.parse_image(html, category)
+        image_link = parser_utils.parse_image(html, id, category)
         
         uploader.upload_post(ref, id, category, title, body, date, image_link)
