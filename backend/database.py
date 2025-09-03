b35378d7b34e491fb039825efd51633240c9a109
Sarang Mittal <sm.sarang09@gmail.com>
2025-09-03 22:05:34 +0530

    changed file structuring to make deployment work

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, firestore
 from pathlib import Path
 
 
-key_path = Path(__file__).resolve().parent.parent / "data" / "serviceAccountKey.json"
+key_path = Path(__file__).resolve().parent / "data" / "serviceAccountKey.json"
 
 cred = credentials.Certificate(key_path)
 firebase_admin.initialize_app(cred)
