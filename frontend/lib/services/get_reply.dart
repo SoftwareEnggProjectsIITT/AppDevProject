5f31d222e60ff4c4f11905b4a571671d8d25aaf5
Nillanjan Singh <nillanjan1604@gmail.com>
2025-09-05 18:49:54 +0530

    include RAG

--- frontend/lib/services/get_reply.dart
+++ frontend/lib/services/get_reply.dart
@@ -1,11 +1,11 @@
 import 'package:frontend/providers/notifiers.dart';
-import 'package:frontend/services/gemini_service.dart';
+import 'package:frontend/services/reply_service.dart';
 import 'package:frontend/services/manage_messages.dart';
 
 Future<String> getReply(String query) async {
   final GeminiService geminiService = GeminiService();
   late String reply;
-  reply = await geminiService.getReply(query);
+  reply = await geminiService.getData(query);
   await sendMessage(convNotifier.value!, reply, "ai");
   return reply;
 }
